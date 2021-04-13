72cd2d662841c66a228c8e48fe121596c5f13369
narbaysebas@gmail.com <narbaysebas@gmail.com>
2021-04-12 21:44:18 -0300

    Agrego gitignore Creo la home view para mostrar un str. Se crean los objs dentro de los metodos, claramente 0 extensible

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using NetCore_DI.Models;
+using NetCore_DI.Models.ViewModels;
+using NetCore_DI.Services;
 
 namespace NetCore_DI.Controllers
 {
@@ -20,7 +22,25 @@ namespace NetCore_DI.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            HomeVM hVm = new HomeVM();
+            MarketForecaster mForecaster = new MarketForecaster();
+            var currentMarket = mForecaster.GetMarketPrediction();
+            switch(currentMarket.MarketCondition)
+            {
+                case MarketCondition.StableDown:
+                    hVm.MarketForecast ="It's crashing down!!!!";
+                    break;
+                case MarketCondition.Volatile:
+                    hVm.MarketForecast ="It's all over the place!";
+                    break;
+                case MarketCondition.StableUp:
+                    hVm.MarketForecast ="Bull run jump aboard";
+                    break;
+                default:
+                    hVm.MarketForecast ="What's going on here!!!";
+                    break;                                                            
+            }
+            return View(hVm);
         }
 
         public IActionResult Privacy()
---
+++ Models/ViewModels/HomeVM.cs
@@ -0,0 +1,9 @@
+namespace NetCore_DI.Models.ViewModels
+{
+    public class HomeVM
+    {
+        public string MarketForecast { get; set; }
+        
+        
+    }
+}
